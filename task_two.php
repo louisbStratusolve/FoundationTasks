73677f2277de18641540eba344c3595727fa1e28
louisbStratusolve <louis.barnard@stratusolve.com>
2023-06-12 16:43:26 +0200

    Recursive function fixes

--- task_two.php
+++ task_two.php
@@ -1,16 +1,16 @@
 <?php
 function getFibonacciSequence($NumberEnteredInt) {
-    if($NumberEnteredInt<0){
+    if ($NumberEnteredInt < 0) {
         return [];
     }
-    $ResultArr = [0,1];
+    $ResultArr = [0, 1];
     $CurrentNumber = 1;
-    while(true){
+    while (true) {
         $LengthInt = count($ResultArr);
-        $CurrentNumber = ($ResultArr[$LengthInt - 1] + $ResultArr[$LengthInt- 2]);
-        if($CurrentNumber <= $NumberEnteredInt) {
+        $CurrentNumber = ($ResultArr[$LengthInt - 1] + $ResultArr[$LengthInt - 2]);
+        if ($CurrentNumber <= $NumberEnteredInt) {
             $ResultArr[] = $CurrentNumber;
-        }else{
+        } else {
             break;
         }
     }
@@ -21,24 +21,24 @@ function getFibonacciSequence($NumberEnteredInt) {
 // TODO: RECURSIVE CODE MAKE TOTALLY RECURSIVE
 
 
-    function getFibonacciByIndexRecursive($LastNumberInt) {
-        if ($LastNumberInt <= 0) {
-            return [];
-        } else if ($LastNumberInt == 1) {
-            return [0];
-        } else {
-            $ResultArr = getFibonacciSequence($LastNumberInt - 1);
-            $length = count($ResultArr);
-            $nextNumber = $ResultArr[$length - 1] + $ResultArr[$length - 2];
-            if ($nextNumber <= $LastNumberInt) {
-                $ResultArr[] = $nextNumber;
-            }
-            return $ResultArr;
+function getFibonacciByIndexRecursive($NumberEnteredInt) {
+    if ($NumberEnteredInt <= 0) {
+        echo "[]";
+    } if ($NumberEnteredInt == 1) {
+        return [0];
+    } else {
+        $ResultArr = getFibonacciSequence($NumberEnteredInt - 1);
+        $length = count($ResultArr);
+        $nextNumber = $ResultArr[$length - 1] + $ResultArr[$length - 2];
+        if ($nextNumber <= $NumberEnteredInt) {
+            $ResultArr[] = $nextNumber;
         }
+        return $ResultArr;
     }
+}
 
-
-    //echo(json_encode(getFibonacciByIndexRecursive(34)));
-    echo(json_encode(getFibonacciSequence(-5)));
+echo(json_encode(getFibonacciByIndexRecursive(34)));
+echo "\n";
+echo(json_encode(getFibonacciSequence(34)));
 
 ?>
