0479fb69e6cdb56fcf0d77ac7fc08eb400efb7b8
louisbStratusolve <louis.barnard@stratusolve.com>
2023-06-13 09:28:41 +0200

    Cleanup of recursive function, validation check moved to the top

--- task_one.php
+++ task_one.php
@@ -1,20 +1,31 @@
 <?php
 
-function addAllNumbersRecursive($NumbersArr) {
+function addAllNumbersRecursive($NumbersArr, $RemoveFirstBool) {
+    if (count($NumbersArr) <= 1) {
+        return end($NumbersArr);
+    }
     $TotalSumArrayCountInt = 0;
+
     $SumArrayCountInt = 0;
     foreach ($NumbersArr as $NumberInt) {
         $SumArrayCountInt += $NumberInt;
     }
     $TotalSumArrayCountInt += $SumArrayCountInt;
-    array_shift($NumbersArr);
-    if (count($NumbersArr) > 0) {
-        $TotalSumArrayCountInt += addAllNumbersRecursive($NumbersArr);
-    }
+    $NumbersArr = shiftOrPop($NumbersArr, $RemoveFirstBool);
+    $TotalSumArrayCountInt += addAllNumbersRecursive($NumbersArr, $RemoveFirstBool);
+
     return $TotalSumArrayCountInt;
 }
+function shiftOrPop($NumbersArr, $RemoveFirstBool){
+    if ($RemoveFirstBool){
+        array_shift($NumbersArr);
+    }else{
+        array_pop($NumbersArr);
+    }
+    return $NumbersArr;
+}
 
-function addAllNumbers($NumbersArr) {
+function addAllNumbers($NumbersArr, $RemoveFirstBool) {
     $TotalSumArrayCountInt = 0;
 
     while (count($NumbersArr) > 0) {
@@ -23,13 +34,13 @@ function addAllNumbers($NumbersArr) {
             $SumArrayCountInt += $NumberInt;
         }
         $TotalSumArrayCountInt += $SumArrayCountInt;
-        array_shift($NumbersArr);
+        $NumbersArr = shiftOrPop($NumbersArr, $RemoveFirstBool);
     }
     return $TotalSumArrayCountInt;
 }
 
-$NumbersArr = [1, 1, 1, 1, 1]; //5+4+3+2+1=15
+$NumbersArr = [1, 1, 1, 1, 5]; //5+4+3+2+1=15
+echo addAllNumbers($NumbersArr, true);
+echo addAllNumbersRecursive($NumbersArr, true);
 
-echo addAllNumbers($NumbersArr);
-echo addAllNumbersRecursive($NumbersArr);
 ?>
